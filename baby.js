807ba998e30010cf5293a46cb46b11ee0b5a382c
Philliph Drummond <oilsinwater@gmail.com>
2018-05-19 22:36:36 -0700

    updates baby.js and crypto.js with psuedo code

--- baby.js
+++ baby.js
@@ -1,10 +1,15 @@
 'use strict'
 
-var result = 0;
+// declaring some result and assigning it to 0
+var result = 0; 
 
-for (i = 2; i < process.argv.length; i++) {
+// loop through the length of the process argument
+for (i = 2; i < process.argv.length; i++) { 
+
+  // add the number of the process.argv to the result, and assign it to the result
   result += Number(process.argv[i]);
 }
 
+// print the result to the screen
 console.log(result);
 
--- cyrpto.js
+++ cyrpto.js
@@ -1,7 +1,6 @@
 // not in the workshop
 'use strict'
-
+// require the crypto module
 let crypto = require('crypto');
 
 crypto.createHash('md5').update('hello world, you can kiss my grits!').digest('hex');
-
